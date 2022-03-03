2b7395090bbec7b3a0848746392202622db5a564
Keaton <me@keatonpalmer.com>
2022-03-03 16:50:43 -0500

    Create assignments.js

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,27 @@
+let COUNTRY = "USA";
+let CONTINENT = "North America";
+let population = 579000000;
+
+console.log(COUNTRY, CONTINENT, population);
+
+
+let isIsland = false;
+let LANGUAGE = "English";
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof COUNTRY);
+console.log(typeof LANGUAGE);
+
+
+console.log(population/2);
+console.log(population+1);
+
+let finlandPopulation = 6000000;
+
+if (population > finlandPopulation) {
+    console.log("The " + COUNTRY + " has a larger population than Finland");
+} else {
+    (console.log("Finland has a greater population than the " + COUNTRY));
+}
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Keaton');
+console.log(30);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+
+let person = 'Keaton';
+let PI = 3.14;
+
+let myFirstJob = 'Cashier';
+let myCurrentJob = 'Information Systems Support Analyst';
+
+console.log(myFirstJob);
